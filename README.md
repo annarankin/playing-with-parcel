6e3176cbda1ae000bc5d42f13cfec5751334c659
Anna Rankin <anna@annarankin.com>
2018-05-28 15:55:09 -0400

    adds instructions

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pixels with Parcel
+
+## Setup
+
+```
+git clone git@github.com:annarankin/playing-with-parcel.git
+cd playing-with-parcel
+yarn
+```
+
+## Start
+
+```
+yarn start
+```
--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,12 +1,5 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import App from './app/app'
-import 'antd/dist/antd.css'
 
-const HalloWorld: React.SFC = () => (
-  <div>
-    <App/>
-  </div>
-)
-
-render(<HalloWorld/>, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
